5358e52aa2170cb9311cc3576736874599747717
akkradet <akkradet7@gmail.com>
2022-07-06 08:10:53 +0700

    Generated at 07/06/2022 Wed  8:10:53 ICT

--- GenerateAll.bat
+++ GenerateAll.bat
@@ -11,9 +11,9 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /y /f D:\Git\epg_encrypted\aisplay.config.xml "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\aisplay.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
-xcopy /y /f "%USERPROFILE%\AppData\Local\WebGrab+Plus\aisplay.ais.co.th.epg.xml" .
+xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\aisplay.ais.co.th.epg.xml" .
 tar zcvf aisplay.ais.co.th.epg.xml.gz aisplay.ais.co.th.epg.xml
 echo Generated successfully. >> GeneratedDate
 echo Website: aisplay.ais.co.th >> GeneratedDate
@@ -29,9 +29,9 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /y /f D:\Git\epg_encrypted\trueid.config.xml "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\trueid.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
-xcopy /y /f "%USERPROFILE%\AppData\Local\WebGrab+Plus\tv.trueid.net.epg.xml" .
+xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\tv.trueid.net.epg.xml" .
 tar zcvf tv.trueid.net.epg.xml.gz tv.trueid.net.epg.xml
 echo Generated successfully. >> GeneratedDate
 echo Website: tv.trueid.net >> GeneratedDate
@@ -47,9 +47,9 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /y /f D:\Git\epg_encrypted\gigatv.config.xml "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\gigatv.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
-xcopy /y /f "%USERPROFILE%\AppData\Local\WebGrab+Plus\gigatv.3bb.co.th.epg.xml" .
+xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\gigatv.3bb.co.th.epg.xml" .
 tar zcvf gigatv.3bb.co.th.epg.xml.gz gigatv.3bb.co.th.epg.xml
 echo Generated successfully. >> GeneratedDate
 echo Website: gigatv.3bb.co.th >> GeneratedDate
