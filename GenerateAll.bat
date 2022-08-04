9a505d1e20202ae06e4751135e15aa8d36f40702
akkradet <akkradet7@gmail.com>
2022-08-04 11:43:06 +0700

    Generated at 08/04/2022 Thu 11:43:06 ICT

--- GenerateAll.bat
+++ GenerateAll.bat
@@ -11,7 +11,7 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\aisplay.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\aisplay\WebGrab++.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
 xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\aisplay.ais.co.th.epg.xml" .
 tar zcvf aisplay.ais.co.th.epg.xml.gz aisplay.ais.co.th.epg.xml
@@ -29,7 +29,7 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\trueid.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\trueid\WebGrab++.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
 xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\tv.trueid.net.epg.xml" .
 tar zcvf tv.trueid.net.epg.xml.gz tv.trueid.net.epg.xml
@@ -47,13 +47,31 @@ echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
 
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
-xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\gigatv.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml"
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\gigatv\WebGrab++.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\"
 "C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
 xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\gigatv.3bb.co.th.epg.xml" .
 tar zcvf gigatv.3bb.co.th.epg.xml.gz gigatv.3bb.co.th.epg.xml
 echo Generated successfully. >> GeneratedDate
 echo Website: gigatv.3bb.co.th >> GeneratedDate
 echo Name: gigatv.3bb.co.th >> GeneratedDate
+git add .
+git commit -m "Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% ICT"
+git push
+
+cd D:\Git\epg
+echo %date:~10,4%-%date:~4,2%-%date:~7,2% >> GeneratedDate
+
+echo Script started at %time:~0,2%:%time:~3,2%:%time:~6,2% >> GeneratedDate
+
+cd %USERPROFILE%\AppData\Local\WebGrab+Plus
+del %USERPROFILE%\AppData\Local\WebGrab+Plus\WebGrab++.config.xml /Q
+xcopy /S /I /Q /Y /F "D:\Git\epg_encrypted\tvsmagazine\WebGrab++.config.xml" "%USERPROFILE%\AppData\Local\WebGrab+Plus\"
+"C:\Program Files (x86)\WebGrab+Plus\bin\WebGrab+Plus.exe"
+xcopy /S /I /Q /Y /F "%USERPROFILE%\AppData\Local\WebGrab+Plus\tvsmagazine.com.epg.xml" .
+tar zcvf tvsmagazine.com.epg.xml.gz tvsmagazine.com.epg.xml
+echo Generated successfully. >> GeneratedDate
+echo Website: tvsmagazine.com >> GeneratedDate
+echo Name: tvsmagazine.com >> GeneratedDate
 git add .
 git commit -m "Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% ICT"
 git push
